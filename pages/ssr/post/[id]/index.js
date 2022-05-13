d72f3dddba95fb24cfa32eb7ade2bccbcb2ad46e
miron-developer <miron.arystan9@gmail.com>
2022-05-13 17:40:14 +0600

    add ssr

---
+++ pages/ssr/post/[id]/index.js
@@ -0,0 +1,39 @@
+import Header from "../../../../components/header";
+import Head from "next/head";
+
+const image =
+  "https://dachverband-werder.de/wp-content/uploads/2021/12/public-awareness.jpg";
+
+export async function getServerSideProps({ query }) {
+  let post = await fetch(
+    `https://gorest.co.in/public/v2/posts/${query?.id}`
+  ).then((resp) => resp.json());
+
+  return { props: { ...post } };
+}
+
+const Post = ({ id, title, body }) => {
+  return (
+    <>
+      <Header />
+
+      <Head>
+        <title>{title} | SEO-Test</title>
+
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={body} />
+        <meta property="og:image" content={image} />
+
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={body} />
+        <meta name="twitter:image" content={image} />
+      </Head>
+
+      <h1>Post: {id}</h1>
+      <h2>Title: {title}</h2>
+      <p>Body: {body}</p>
+    </>
+  );
+};
+
+export default Post;
